498535e6d4b0179ebbb453a4fa89b6e22a3bc94a
Abi Raja <abimanyuraja@gmail.com>
2011-06-08 16:26:44 -0700

    Setup `a` command.

--- setup.sh
+++ setup.sh
@@ -11,6 +11,13 @@ mkdir -p ~/.vim/tmp
 # Vim color schemes setup (this should really be automated)
 mkdir -p ~/.vim/colors
 
+# Install the `a` command (a CLI for anything that I want to do in path-agnostic manner)
+echo "python `pwd`/a.py \$*" > /usr/bin/a
+chmod a+x /usr/bin/a
+# Install the necessary software to run the dev tools
+pip install pyyaml==3.09
+pip install termcolor==1.1.0
+
 #apt-get install vim-ruby
 
 #Vim Vundle setup
